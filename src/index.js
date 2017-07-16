0a64d354ac1bfddee097de4bf1e1d91ac92d4ce5
cystiger <cysyystiger@gmail.com>
2017-07-16 21:31:04 +0800

    de

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import ReactDom from 'react-dom';
-import TodoApp from './components/TodoApp';
-
-
-
-ReactDom.render(<TodoApp />, document.querySelector('.container'));
