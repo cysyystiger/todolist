ad3c05ba8d3fa8d50f20840bc5c68c1ccb592a8e
cystiger <cysyystiger@gmail.com>
2017-07-16 21:22:10 +0800

    birth

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import TodoApp from './components/TodoApp';
+
+
+
+ReactDom.render(<TodoApp />, document.querySelector('.container'));
